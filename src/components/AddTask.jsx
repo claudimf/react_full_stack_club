a707c358708ec24a91dfe4aa143f0871a2396749
Claudi <claudimartinsdefreitas@gmail.com>
2025-01-16 08:13:24 -0300

    Adding component 'addTask'

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,20 @@
 function AddTask() {
     return (
-        <h1>Add Task</h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input 
+                type="text" 
+                placeholder="Digite o título da tarefa"
+                className="border border-slate-300 outline-stone-400 px-4 py-2 rounded-md"
+            />
+            <input 
+                type="text" 
+                placeholder="Digite a descrição da tarefa"
+                className="border border-slate-300 outline-stone-400 px-4 py-2 rounded-md"
+            />
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md">
+                Adicionar
+            </button>
+        </div>
     );
 }
 
