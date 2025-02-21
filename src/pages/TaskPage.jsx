83f352db6893c90f204502b348a0f1c865fd45b9
Claudi <claudimartinsdefreitas@gmail.com>
2025-02-21 18:52:56 -0300

    Using query params

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,5 +1,23 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useSearchParams } from "react-router-dom";
+
 function TaskPage() {
-    return <h1>task page</h1>;
+    const [searchParams] = useSearchParams();
+    const title = searchParams.get("title")
+    const description = searchParams.get("description")
+
+    return <div className="h-screen w-screen bg-slate-500 p-6">
+        <div className="w-[500px] mx-auto space-y-4">
+            <div className="flex justify-center relative mb-6">
+                Detalhes da Tarefa
+            </div>
+
+            <div className="bg-slate-200 p-4 rounded-md">
+                <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+                <p className="text-slate-600">{description}</p>
+            </div>
+        </div>
+    </div>;
 }
 
 export default TaskPage;
